8133c42bd5b5298a5387de0f4730b80a449c22a8
John Sprinkle <john.sprinkle@tuta.io>
2022-02-20 21:08:35 -0500

    Workaround to allow docker-compose images to be updated by dependabot

--- start.sh
+++ start.sh
@@ -2,6 +2,12 @@
 
 projectName=${PROJECT_NAME:-dns}
 
+UNBOUND_IMAGE=$(cat dependabot-dockerfiles/unbound/Dockerfile | cut -d ' ' -f 2)
+export UNBOUND_IMAGE
+
+PIHOLE_IMAGE=$(cat dependabot-dockerfiles/pihole/Dockerfile | cut -d ' ' -f 2)
+export PIHOLE_IMAGE
+
 docker-compose -p "${projectName}" -f docker-compose.yml pull
 docker-compose -p "${projectName}" -f docker-compose.yml -f docker-compose.ports.yml up -d
 
