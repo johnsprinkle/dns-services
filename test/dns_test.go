deaaaea5ce1a0df9bdd16b60427ec3de06aa6ac3
John Sprinkle <john.sprinkle@tuta.io>
2022-02-22 20:54:00 -0500

    Fix failing test

--- test/dns_test.go
+++ test/dns_test.go
@@ -9,7 +9,7 @@ import (
 func TestDNSLookup(t *testing.T) {
 	var dig dnsutil.Dig
 	dig.Retry = 5
-	err := dig.At("127.0.0.1:5054")
+	err := dig.At("127.0.0.1:5053")
 	handleError(err)
 	a, err := dig.A("google.com")
 	handleError(err)
