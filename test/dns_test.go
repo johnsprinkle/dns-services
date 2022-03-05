3bce1a1cbbbe08f5c2cfb192e29fddfde121eda3
John Sprinkle <john.sprinkle@tuta.io>
2022-03-04 20:57:05 -0500

    Enable host networking and make it the default

--- test/dns_test.go
+++ test/dns_test.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"github.com/lixiangzhong/dnsutil"
 	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
 )
 
 func TestDNSLookup(t *testing.T) {
 	var dig dnsutil.Dig
 	dig.Retry = 5
-	err := dig.At("127.0.0.1:5053")
+	err := dig.At(fmt.Sprintf("127.0.0.1:%s", os.Getenv("PIHOLE_HOST_PORT")))
 	handleError(err)
 	a, err := dig.A("google.com")
 	handleError(err)
