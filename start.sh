f467c33318158f0860206571a22050231d872411
John Sprinkle <john.sprinkle@tuta.io>
2022-03-04 20:57:05 -0500

    Enable host networking and make it the default

--- start.sh
+++ start.sh
@@ -9,6 +9,6 @@ PIHOLE_IMAGE=$(cat dependabot-dockerfiles/pihole/Dockerfile | cut -d ' ' -f 2)
 export PIHOLE_IMAGE
 
 docker-compose -p "${projectName}" -f docker-compose.yml pull
-docker-compose -p "${projectName}" -f docker-compose.yml -f docker-compose.ports.yml up -d
+docker-compose -p "${projectName}" -f docker-compose.yml -f docker-compose.host-networking.yml up -d
 
 docker ps
